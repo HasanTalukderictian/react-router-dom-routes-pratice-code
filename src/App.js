81b6ad4405d58fb4d0366af53ca36d2f3b910e40
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-07-23 00:05:31 +0600

    Header and Link Add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './Compoents/Home/Home';
+import About from './Compoents/About/About';
+import Products from './Compoents/Products/Products';
 
 function App() {
     const router = createBrowserRouter([
         {path:'', element:<div>This is default Page</div>},
-        {path:'/home', element:<div>Home</div>},
-        {path:'/about', element:<div>This is About Page</div>},
+        {path:'/home', element:<Home></Home>},
+        {path:'/about', element:<About></About>},
+        {path:'/products',element:<Products></Products>}
 
     ])
 
